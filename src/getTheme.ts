8a31a21a0998759890f1041df2d1d1cc40ebb0c4
Oleg La <oleglambin@gmail.com>
2023-07-13 20:20:20 +0300

    fix getTheme

--- src/getTheme.ts
+++ src/getTheme.ts
@@ -1,35 +1,43 @@
-import mobimed from "./mobimed";
-import drAnna from "./drAnna";
-import gms from "./gms";
-import gmswiget from "./gmswiget";
-import medincenter from "./medincenter";
-import medswiss from "./medswiss";
-import mediadoc from "./mediadoc";
-import minfin from "./minfin";
-import pimu from "./pimu";
-import ncn from "./ncn";
-import sibgmu from "./sibgmu";
+// import mobimed from "./mobimed";
+// import drAnna from "./drAnna";
+// import gms from "./gms";
+// import gmswiget from "./gmswiget";
+// import medincenter from "./medincenter";
+// import medswiss from "./medswiss";
+// import mediadoc from "./mediadoc";
+// import minfin from "./minfin";
+// import pimu from "./pimu";
+// import ncn from "./ncn";
+// import sibgmu from "./sibgmu";
+
+// const customSettings = async () => {
+//   const custom = (await import(`./${process.env.REACT_APP_THEME}`))
+//     .default;
 
 export default function getTheme(name?: string) {
-  return name === "drAnna"
-    ? drAnna
-    : name === "gms"
-    ? gms
-    : name === "gmswiget"
-    ? gmswiget
-    : name === "medincenter"
-    ? medincenter
-    : name === "medswiss"
-    ? medswiss
-    : name === "mediadoc"
-    ? mediadoc
-    : name === "minfin"
-    ? minfin
-    : name === "pimu"
-    ? pimu
-    : name === "ncn"
-    ? ncn
-    : name === "sibgmu"
-    ? sibgmu
-    : mobimed;
+  return async () => {
+    return await import(`./${name}`);
+  };
+
+  // return name === "drAnna"
+  //   ? drAnna
+  //   : name === "gms"
+  //   ? gms
+  //   : name === "gmswiget"
+  //   ? gmswiget
+  //   : name === "medincenter"
+  //   ? medincenter
+  //   : name === "medswiss"
+  //   ? medswiss
+  //   : name === "mediadoc"
+  //   ? mediadoc
+  //   : name === "minfin"
+  //   ? minfin
+  //   : name === "pimu"
+  //   ? pimu
+  //   : name === "ncn"
+  //   ? ncn
+  //   : name === "sibgmu"
+  //   ? sibgmu
+  //   : mobimed;
 }
