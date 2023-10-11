5db94ac46465b33f40ea4286b55787914d614570
Oleg La <oleglambin@gmail.com>
2023-10-11 11:45:38 +0300

    fix: skyfert

--- src/skyfert/overrides/Rating.ts
+++ src/skyfert/overrides/Rating.ts
@@ -6,14 +6,17 @@ export default function Rating(theme: Theme) {
   return {
     MuiRating: {
       styleOverrides: {
+        icon: {
+          color: theme.palette.primary.main
+        },
         root: {
           color: theme.palette.primary.main,
           "& span > span": {
             top: 0,
-            left: 0,
-          },
-        },
-      },
-    },
+            left: 0
+          }
+        }
+      }
+    }
   };
 }
